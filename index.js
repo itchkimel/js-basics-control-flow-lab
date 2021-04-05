90dad620bde26e5056c8b59b38337ba59c00fb5b
Yitzchok Kimelman <itchkimel@gmail.com>
2021-04-05 19:08:58 -0400

    commit tree 8f0b7a7c2792d56cd17c4d9b62a45214cfc18e8e

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(n){
+  // 1 check the number if le4ss than 400 return ...
+  if (n <= 400){
+    return `This one is on me!`;
+  }
+  // 2 if request (argument) if above 2000 but less that 2500 return...
+  else if (n >= 2000 && n < 2500){
+    return `I will gladly take your thirty bucks.`;
+  }
+  else {
+    return `No can do.`
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(s){
   // Write your code here!
+  return (s === `NYC` ? `Ok, sounds good.` : `No go.`)
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case `generous`:
+      return `Thank you so much.`;
+      break;
+    case `not as generous`:
+      return `Thank you.`;
+      break;
+    case `thanks for everything`:
+      return `Bye.`;
+      break
+  }
 }
